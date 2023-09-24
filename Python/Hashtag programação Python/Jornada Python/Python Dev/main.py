a835847e59f3c2c9c1e80d07b08fac812198128e
Samuel Campelo <samuelcampelo25@gmail.com>
2023-09-24 20:31:18 -0300

    Backend do Hashzap.

---
+++ Python/Hashtag programação Python/Jornada Python/Python Dev/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on("message")
+def gerenciar_mensagem(mensagem):
+    send(mensagem, broadcast=True)
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+socketio.run(app, host="192.168.100.8")
