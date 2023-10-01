cdb02c597b8388419afd9eecb18a20834e3a9349
Samuel Campelo <samuelcampelo25@gmail.com>
2023-09-30 22:48:36 -0300

    Tabula.

--- Python/Curso em vídeo Python/Mundo 3/Aula 22 - Módulos e Pacotes/Desafio 110/moeda.py
+++ Python/Curso em vídeo Python/Mundo 3/Aula 22 - Módulos e Pacotes/Desafio 110/moeda.py
@@ -46,8 +46,8 @@ def resumo(preço = 0, porcentagemMais = 0, porcentagemMenos = 0):
     print('RESUMO DOS VALORES'.center(33))
     print('='*33)
     
-    print(f'Preço analisado: {moeda(preço):>13}')
-    print(f'Dobro do preço: {dobro(preço, True):>15}')
-    print(f'Metade do preço: {metade(preço, True):>13}')
-    print(f'{porcentagemMais}% de aumento: {aumentar(preço, porcentagemMais, True):>14}')
-    print(f'{porcentagemMenos}% de redução: {diminuir(preço, porcentagemMenos, True):>14}')
+    print(f'Preço analisado: \t{moeda(preço)}')
+    print(f'Dobro do preço: \t{dobro(preço, True)}')
+    print(f'Metade do preço: \t{metade(preço, True)}')
+    print(f'{porcentagemMais}% de aumento: \t{aumentar(preço, porcentagemMais, True)}')
+    print(f'{porcentagemMenos}% de redução: \t{diminuir(preço, porcentagemMenos, True)}')
