763996a5dd0673d83fbba4e3928ce37c561e4e47
Samuel Campelo <samuelcampelo25@gmail.com>
2023-10-01 22:49:45 -0300

    Update Desafio 007.py

--- Python/Curso em vídeo Python/Mundo 1/Aula 7 - Operadores Aritméticos/Desafio 007.py
+++ Python/Curso em vídeo Python/Mundo 1/Aula 7 - Operadores Aritméticos/Desafio 007.py
@@ -1,4 +1,4 @@
 número1 = float(input('Digite a primeira nota do aluno: '))
 número2 = float(input('Digite a segunda nota do notas: '))
 
-print(f'A média da suas notas é {(número1 + número2) / 2:.2f}.')
+print(f'A média da suas notas é {(número1 + número2) / 2:.1f}.')
