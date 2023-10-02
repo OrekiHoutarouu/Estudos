3fd0777f340d5c62ae9a01830ee8629e0552c871
Samuel Campelo <samuelcampelo25@gmail.com>
2023-10-01 23:03:27 -0300

    Melhora a semântica do desafio 36.

--- Python/Curso em vídeo Python/Mundo 2/Aula 12 - Condições Aninhadas/Desafio 036.py
+++ Python/Curso em vídeo Python/Mundo 2/Aula 12 - Condições Aninhadas/Desafio 036.py
@@ -1,8 +1,11 @@
-v = float(input('Digite o valor da casa: R$'))
-s = float(input('Digite o salário: R$'))
-a = int(input('Digite em quantos anos será paga a casa: '))
-p = v/(a*12)
-if p > s*0.30:
+valor = float(input('Digite o valor da casa: R$'))
+salário = float(input('Digite o salário: R$'))
+anos = int(input('Digite em quantos anos será paga a casa: '))
+
+preço = valor / (anos * 12)
+
+if preço > salário * 0.30:
     print('O empréstimo foi negado pois a prestação mensal ultrapassa o salário em 30% ou mais.')
+    
 else:
-    print(f'O empréstimo foi aceito! O comprador terá que pagar {p:.2f} por mês')
+    print(f'O empréstimo foi aceito! O comprador terá que pagar {preço:.2f} por mês.')
