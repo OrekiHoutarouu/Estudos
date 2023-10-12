ce4c84341db62f21dd13ee986302048cd5e74897
Samuel Campelo <samuelcampelo25@gmail.com>
2023-10-12 10:53:34 -0300

    Update Desafio008.py

--- Python3/Curso_em_video/Mundo1/Aula7_Operadores_aritmeticos/Desafio008.py
+++ Python3/Curso_em_video/Mundo1/Aula7_Operadores_aritmeticos/Desafio008.py
@@ -1,3 +1,3 @@
 metragem = float(input('Digite a metragem de algo: '))
 
-print(f'Essa metragem em centímetros é igual a {metragem * 100:.2f} e em milímetros é igual a {metragem * 1000:.2f}.')
+print(f'Essa metragem em centímetros é igual a {metragem * 100:.2f}cm e em milímetros é igual a {metragem * 1000:.2f}mm.')
