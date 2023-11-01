3ef3a77cfc1869b70cd6d4fbd77e3d86c01b3b55
Samuel Campelo <samuelcampelo25@gmail.com>
2023-11-01 20:51:34 -0300

    Compara x e y em C

--- C/Harvard/CS50/Week1/compare.c
+++ C/Harvard/CS50/Week1/compare.c
@@ -1,4 +1,4 @@
-#include "cs50.h"
+#include "cs50.c"
 #include <stdio.h>
 
 int main(void)
@@ -6,8 +6,16 @@ int main(void)
     int x = get_int("What's x? ");
     int y = get_int("What's y? ");
     
-    if(x < y)
+    if (x < y)
     {
         printf("x is less than y\n");
+    }
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y");
     }
 }
