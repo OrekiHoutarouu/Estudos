d8ab6defed7b082b663993964df3a31e952684ee
Samuel Campelo <samuelcampelo25@gmail.com>
2023-11-27 21:00:38 -0300

    Bubble sorting em C

---
+++ C/Harvard/CS50/Week3/Practice_Problems_3/temps/temps.c
@@ -0,0 +1,90 @@
+#include "libs/cs50.h"
+#include <stdio.h>
+
+#define NUM_CITIES 10
+
+typedef struct
+{
+    string city;
+    int temp;
+}
+avg_temp;
+
+avg_temp temps[NUM_CITIES];
+
+void sort_cities(void);
+
+int main(void)
+{
+    temps[0].city = "Austin";
+    temps[0].temp = 97;
+
+    temps[1].city = "Boston";
+    temps[1].temp = 82;
+
+    temps[2].city = "Chicago";
+    temps[2].temp = 85;
+
+    temps[3].city = "Denver";
+    temps[3].temp = 90;
+
+    temps[4].city = "Las Vegas";
+    temps[4].temp = 105;
+
+    temps[5].city = "Los Angeles";
+    temps[5].temp = 82;
+
+    temps[6].city = "Miami";
+    temps[6].temp = 97;
+
+    temps[7].city = "New York";
+    temps[7].temp = 85;
+
+    temps[8].city = "Phoenix";
+    temps[8].temp = 107;
+
+    temps[9].city = "San Francisco";
+    temps[9].temp = 66;
+
+    sort_cities();
+
+    printf("\nAverage July Temperatures by City\n\n");
+
+    for (int i = 0; i < NUM_CITIES; i++)
+    {
+        printf("%s: %i\n", temps[i].city, temps[i].temp);
+    }
+}
+
+void sort_cities(void)
+{
+    int swaps = 1;
+    int firstTemperature = 0;
+    string firstCity = " ";
+
+    while (swaps != 0)
+    {
+        swaps = 0;
+        for (int i = 0; i < NUM_CITIES; i++)
+        {
+            if (temps[i + 1].temp == 0)
+            {
+                break;
+            }
+
+            else if (temps[i].temp < temps[i + 1].temp)
+            {
+                firstTemperature = temps[i].temp;
+                firstCity = temps[i].city;
+
+                temps[i].temp = temps[i + 1].temp;
+                temps[i].city = temps[i + 1].city;
+
+                temps[i + 1].temp = firstTemperature;
+                temps[i + 1].city = firstCity;
+
+                swaps += 1;
+            }
+        }
+    }
+}
