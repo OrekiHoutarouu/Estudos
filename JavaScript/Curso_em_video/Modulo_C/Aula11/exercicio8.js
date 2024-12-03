e6dd9e4e904dce05be5079f047a6955acd0c1aaf
Samuel Campelo <samuelcampelo25@gmail.com>
2024-12-03 17:12:58 -0300

    Condicional simples

---
+++ JavaScript/Curso_em_video/Modulo_C/Aula11/exercicio8.js
@@ -0,0 +1,8 @@
+var velocidade = 79.9
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+
+if (velocidade > 60) {
+    console.log("Você ultrapassou o limite de velocidade. MULTADO!")
+}
+
+console.log("Dirija sempre usando cinto de segurança")
