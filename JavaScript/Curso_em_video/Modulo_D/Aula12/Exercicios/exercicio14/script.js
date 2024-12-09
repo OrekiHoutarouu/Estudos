e3c33515218b78dd2f20d7f3c6e112e163d3a522
Samuel Campelo <samuelcampelo25@gmail.com>
2024-12-09 17:14:41 -0300

    Finaliza o site

--- JavaScript/Curso_em_video/Modulo_D/Aula12/Exercicios/exercicio14/script.js
+++ JavaScript/Curso_em_video/Modulo_D/Aula12/Exercicios/exercicio14/script.js
@@ -1,1 +1,20 @@
-alert("Olá")
+function carregar() {
+    var mensagem = window.document.getElementById("mensagem")
+    var imagem = window.document.getElementById("imagem")
+    var hora = (new Date()).getHours() + ":" + (new Date()).getMinutes()
+
+    if (hora >= 0 && hora <= 11) {
+        imagem.src = "imagens/manha.jpg"
+        document.body.style.background = "#7696a5"
+
+    } else if (hora >= 12 && hora <= 17) {
+        imagem.src = "imagens/tarde.jpg"
+        document.body.style.background = "#c9641f"
+
+    } else {
+        imagem.src = "imagens/noite.jpg"
+        document.body.style.background = "#303133"
+    }
+
+    mensagem.innerHTML = `Agora são ${hora} horas`
+}
