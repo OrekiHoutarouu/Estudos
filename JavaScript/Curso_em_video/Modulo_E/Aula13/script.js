8c756aaa990b09c774eb21c6bcf5f3508be2ed76
Samuel Campelo <samuelcampelo25@gmail.com>
2024-12-11 15:12:28 -0300

    Testa tipos de repetição

---
+++ JavaScript/Curso_em_video/Modulo_E/Aula13/script.js
@@ -0,0 +1,17 @@
+var contador = 1
+
+while (contador < 6) {
+    console.log(`Olá ${contador}`)
+    contador++
+}
+
+contador = 1
+
+do {
+    console.log(`Tudo ${contador}`)
+    contador++
+} while (contador < 6)
+
+for (var contador2 = 1; contador2 < 6; contador2++) {
+    console.log(`Bem? ${contador2}`)
+}
