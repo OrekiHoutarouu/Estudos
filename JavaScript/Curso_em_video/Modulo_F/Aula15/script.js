b81cc53941a3f0a9a0862e6e662a2a4956a7567e
Samuel Campelo <samuelcampelo25@gmail.com>
2024-12-15 16:31:32 -0300

    Testa arrays

---
+++ JavaScript/Curso_em_video/Modulo_F/Aula15/script.js
@@ -0,0 +1,6 @@
+let numeros = [5, 8, 2, 9, 3]
+numeros.sort()
+
+console.log(`Nosso array é o ${numeros}`)
+console.log(`O array tem ${numeros.length} elementos`)
+console.log(`O primeiro dígito do array é ${numeros[0]}`)
