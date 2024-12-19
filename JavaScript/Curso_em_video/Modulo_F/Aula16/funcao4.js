02abeea12356fc75b580a811b6d261eb02da3405
Samuel Campelo <samuelcampelo25@gmail.com>
2024-12-19 16:18:04 -0300

    Função de fatorial

---
+++ JavaScript/Curso_em_video/Modulo_F/Aula16/funcao4.js
@@ -0,0 +1,11 @@
+function fatorial(numero) {
+    let fatorial = 1 
+    
+    for (let contador = numero; contador > 1; contador--) {
+        fatorial *= contador
+    }
+
+    return fatorial
+}
+
+console.log(fatorial(5))
