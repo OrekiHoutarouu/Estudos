d6759980f09c0e8684632a030e117b455ef075c5
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-13 17:19:53 -0300

    Testa comentários

---
+++ JavaScript/Udemy/Secao2/aula4/comentario.js
@@ -0,0 +1,14 @@
+// Isso é um comentário
+console.log("Olá mundo!") //Aqui tem outro comentário
+console.log("Linha 1")
+console.log("Linha 2")
+
+/*
+Isso aqui é
+um bloco de comentário
+*/
+
+console.log("Linha 3")
+console.log("Linha 4")
+console.log("Linha 5")
+//Isso vai ser ignorado
