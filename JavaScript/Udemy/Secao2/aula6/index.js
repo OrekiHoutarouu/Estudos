a03b4b9748587d1df6bba37632bb33e0f3c007b5
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-14 16:26:36 -0300

    Variáveis com let

---
+++ JavaScript/Udemy/Secao2/aula6/index.js
@@ -0,0 +1,11 @@
+let nome = "Samuel"
+
+console.log(`${nome} nasceu em 1998.`)
+console.log(`Em 2016 ${nome} conheceu Maria.`)
+console.log(`${nome} casou-se com Maria em 2020.`)
+console.log(`Maria teve 1 filho com ${nome} em 2024.`)
+console.log(`O filho de ${nome} se chama Eduardo.`)
+
+nome = "Otávio"
+
+console.log(nome)
