82232dc940210616e68acd0c465c235d7cf41ae4
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-15 18:45:17 -0300

    Operadores aritméticos

---
+++ JavaScript/Udemy/Secao2/aula10/index.js
@@ -0,0 +1,11 @@
+let numero1 = 2
+let numero2 = 10
+let passo = 2
+
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log(numero1 ** numero2)
+console.log(numero1 % numero2)
+console.log(numero1 += passo)
