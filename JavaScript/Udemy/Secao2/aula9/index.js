d94fa07fd167e1abe0b5f01ee5d902caa26f53db
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-15 16:26:44 -0300

    Tipos primitivos

---
+++ JavaScript/Udemy/Secao2/aula9/index.js
@@ -0,0 +1,8 @@
+const nome1 = "Samuel" // String
+const nome2 = 'Samuel' // String
+const nome3 = `Samuel` // String
+const numero1 = 10 // Number
+const numero2 = 10.5 // Number
+let nomeAluno // Undefined
+const sobrenomeAluno = null // Null
+const alunoAprovado = false // Boolean 
