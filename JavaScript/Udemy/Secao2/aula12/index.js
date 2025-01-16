7711ccdd81f08180176ec9f14e719a7f558d077e
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-16 19:08:49 -0300

    Testa strings

---
+++ JavaScript/Udemy/Secao2/aula12/index.js
@@ -0,0 +1,7 @@
+let texto = "Olá mundo!"
+
+console.log(texto[2])
+console.log(texto.indexOf("mundo"))
+console.log(texto.length)
+console.log(texto.toLocaleUpperCase())
+console.log(texto.toLowerCase())
