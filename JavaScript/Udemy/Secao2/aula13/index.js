ab4908213e6ee24a1a72acfd313b948b1f283961
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-22 18:29:37 -0300

    Faz testes com números

---
+++ JavaScript/Udemy/Secao2/aula13/index.js
@@ -0,0 +1,7 @@
+let numero1 = 10
+let numero2 = 2.5748637486374863
+
+console.log(numero1.toString() + numero2.toString())
+console.log(numero1.toString(2))
+console.log(numero2.toFixed(2))
+console.log(Number.isInteger(numero2)) 
