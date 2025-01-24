783a58ce523864b1d5aea671590dee9dd3b61bf9
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-24 09:48:17 -0300

    Adiciona mais testes

--- JavaScript/Udemy/Secao2/aula12/index.js
+++ JavaScript/Udemy/Secao2/aula12/index.js
@@ -1,7 +1,9 @@
 let texto = "Olá mundo!"
 
 console.log(texto[2])
-console.log(texto.indexOf("mundo"))
+console.log(texto.charAt(4))
+console.log(texto.slice(0, 3))
+console.log(texto.indexOf("m"))
 console.log(texto.length)
-console.log(texto.toLocaleUpperCase())
+console.log(texto.toUpperCase())
 console.log(texto.toLowerCase())
