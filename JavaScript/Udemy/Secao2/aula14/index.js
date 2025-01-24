938d35c940674b9a2f9193c5f849be83ea3520bf
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-24 10:12:29 -0300

    Usa objeto math

---
+++ JavaScript/Udemy/Secao2/aula14/index.js
@@ -0,0 +1,11 @@
+let pi = 3.14159265
+
+console.log(Math.floor(pi))
+console.log(Math.ceil(pi))
+console.log(Math.round(pi))
+
+console.log(Math.max(1, 4, 6, -3, 1500, 869, -97))
+console.log(Math.min(1, 4, 6, -3, 1500, 869, -97))
+
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5)
+console.log(aleatorio)
