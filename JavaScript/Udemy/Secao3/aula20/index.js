6ae31a95bdec30559393042fdea8adf991ab2e44
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-29 09:47:42 -0300

    Testa os operadores de comparação

---
+++ JavaScript/Udemy/Secao3/aula20/index.js
@@ -0,0 +1,31 @@
+comparacao = 10 > 5
+
+console.log(comparacao)
+
+comparacao = 5 < 10
+
+console.log(comparacao)
+
+comparacao = 5 <= 5
+
+console.log(comparacao)
+
+comparacao = 5 >= 5
+
+console.log(comparacao)
+
+comparacao = 10 == "10"
+
+console.log(comparacao)
+
+comparacao = 10 === 10
+
+console.log(comparacao)
+
+comparacao = 10 != "5"
+
+console.log(comparacao)
+
+comparacao = 10 !== 5
+
+console.log(comparacao)
