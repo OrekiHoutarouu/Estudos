7ed5c186fdef910bf40d4219706f3848a110b1d9
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-30 13:39:04 -0300

    Testa if e else novamente

---
+++ JavaScript/Udemy/Secao3/aula24/index.js
@@ -0,0 +1,11 @@
+const numero = 12
+
+if (numero >= 0 && numero <= 5) {
+    console.log("O número está entre 0 e 5.")
+} else if (numero >= 6 && numero <= 10) {
+    console.log("O número está entre 6 e 10.")
+} else {
+    console.log("O número não está entre 0 e 10.")
+}
+
+console.log("...Aqui vai o resto do código.")
