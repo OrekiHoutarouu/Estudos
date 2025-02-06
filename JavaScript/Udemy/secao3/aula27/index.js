6079ff92b9ff1bd89e91d48bd9df6b16827cdead
Samuel Campelo <samuelcampelo25@gmail.com>
2025-02-06 15:52:34 -0300

    Trabalha com objeto Date

---
+++ JavaScript/Udemy/secao3/aula27/index.js
@@ -0,0 +1,13 @@
+const data = new Date()
+
+console.log("Dia", data.getDate())
+console.log("Mês", data.getMonth())
+console.log("Ano", data.getFullYear())
+console.log("Hora", data.getHours())
+console.log("Minuto", data.getMinutes())
+console.log("Segundo", data.getSeconds())
+console.log("Milissegundos", data.getMilliseconds())
+console.log("Dia na semana", data.getDay())
+
+
+console.log(data.toString())
