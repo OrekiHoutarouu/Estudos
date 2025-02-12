6a5080282bbc23d59168c1b0be4f4888de339f88
Samuel Campelo <samuelcampelo25@gmail.com>
2025-02-12 19:14:17 -0300

    Testa propriedades do objeto

---
+++ JavaScript/Udemy/secao3/aula31/index.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: "Samuel",
+    sobrenome: "Campelo",
+    idade: 15,
+    endereco: {
+        rua: "Avenida Brasil",
+        numero: 999
+    }
+}
+
+const {nome, sobrenome, idade} = pessoa
+console.log(nome, sobrenome, idade)
