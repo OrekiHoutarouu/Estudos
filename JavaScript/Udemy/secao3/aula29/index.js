6c8170d5ee7ec9bdbfdbaa89b4c328f4048f6d74
Samuel Campelo <samuelcampelo25@gmail.com>
2025-02-12 16:41:20 -0300

    Testa escopo de variáveis

---
+++ JavaScript/Udemy/secao3/aula29/index.js
@@ -0,0 +1,14 @@
+let nome = "Luís"
+var nome2 = "Luís"
+
+if (true) {
+    let nome = "Samuel"
+    console.log(nome, nome2)
+
+    if (true) {
+        let nome = "Campelo"
+        console.log(nome, nome2)
+    }
+}
+
+console.log(nome, nome2)
