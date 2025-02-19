239689bb9714d39dd829b78f12ac56a97e517e1a
Samuel Campelo <samuelcampelo25@gmail.com>
2025-02-19 16:53:23 -0300

    Trabalha com for in

---
+++ JavaScript/Udemy/secao3/aula34/index.js
@@ -0,0 +1,15 @@
+const frutas = ["Pêra", "Maçã", "Uva"]
+
+for (let c in frutas) {
+    console.log(frutas[c])
+}
+
+const pessoa = {
+    nome: "Samuel",
+    sobrenome: "Campelo",
+    idade: 30
+}
+
+for (let c in pessoa) {
+    console.log(c, pessoa[c])
+}
