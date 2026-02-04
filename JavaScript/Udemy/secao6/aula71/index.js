5aaecf3372a5ea9e2702dfcfa8e83d80810abc24
Samuel Campelo <samuelcampelo25@gmail.com>
2026-02-04 15:30:05 -0300

    Usa classes

---
+++ JavaScript/Udemy/secao6/aula71/index.js
@@ -0,0 +1,21 @@
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+
+    falar() {
+        console.log(`${this.nome} está falando`)
+    }
+
+    comer() {
+        console.log(`${this.nome} está comendo`)
+    }
+
+    beber() {
+        console.log(`${this.nome} está bebendo`)
+    }
+}
+
+const pessoa1 = new Pessoa("Samuel", "Campelo")
+pessoa1.comer()
