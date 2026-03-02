526180788d31babfebb3d8be6178d546dd6d46a6
Samuel Campelo <samuelcampelo25@gmail.com>
2026-03-02 03:17:30 -0300

    Utiliza Promises

--- JavaScript/Udemy/secao8/aula77/index.js
+++ JavaScript/Udemy/secao8/aula77/index.js
@@ -1,1 +1,34 @@
-function co
+function cria_numero_aleatorio(min, max) {
+    max *= 1000
+    min *= 1000
+
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function espera(mensagem, tempo) {
+    return new Promise((resolve, reject) => {
+        if (typeof mensagem !== "string") reject("ERRO")
+
+        setTimeout(() => {
+            resolve(mensagem)
+        }, tempo) 
+    })
+}
+
+espera("Frase 1", cria_numero_aleatorio(1, 3))
+    .then(resposta => {
+        console.log(resposta)
+        return espera("Frase 2", cria_numero_aleatorio(1, 3))
+    })
+    .then(resposta => {
+        console.log(resposta)
+        return espera(1, cria_numero_aleatorio(1, 3))
+    })
+    .then(resposta => {
+        console.log(resposta)
+    })
+    .catch(e => {
+        console.log(e)
+    })
+
+console.log("Isso será executado primeiro")
